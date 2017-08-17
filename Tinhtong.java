2f62354786687248dc83da899dbd35c94a29704e
tronganhnguyen <anhnttps04912@fpt.edu.vn>
2017-08-17 07:54:06 +0700

    Add files via upload

---
+++ Tinhtong.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc01
+ */
+import java.util.Scanner;
+public class Tinhtong {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Nhap a");
+        a= sc.nextInt();
+        System.out.println("NHap b");
+        b= sc.nextInt();
+        
+        int c = a + b;
+        //xuat c
+    
+        System.out.println("tong " +c);
+    }
+    
+}
